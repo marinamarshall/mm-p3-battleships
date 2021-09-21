fad384cbca790f17139c551b0f1e499fdc6b8ab4
marinamarshall <marinamarshall2@gmail.com>
2021-09-21 20:12:15 +0000

    Initialize board and call print_battleships_board function

--- run.py
+++ run.py
@@ -2,3 +2,16 @@
 import random
 
 board = []
+
+for x in range(0, 4):
+    board.append(["-"] * 4)
+
+
+def print_battleships_board(board):
+    """
+    Display the game board to the user
+    """
+    for place in board:
+        print(" ".join(place))
+
+print_battleships_board(board)
