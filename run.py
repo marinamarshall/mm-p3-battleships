434105123a61b1a6eb5d0187ae45d88c0fa478ad
marinamarshall <marinamarshall2@gmail.com>
2021-09-21 21:23:36 +0000

    Add user score interactivity

--- run.py
+++ run.py
@@ -4,9 +4,6 @@ import random
 board = []
 miss = []
 
-# Initialize turns to be at zero
-turns = 0
-
 for x in range(0, 4):
     board.append(["-"] * 4)
 
@@ -40,9 +37,11 @@ ship_col = random_col(board)
 print(ship_col)
 
 
-def user_answer():  
+def user_answer():
+    # Initialize turns to be at zero
+    turns = 0
     try:
-        for turn in range(5):
+        for turn in range(10):
             guess_row = int(input("Guess a Row: (between 0 and 3)\n"))
             guess_col = int(input("Guess a Column: (between 0 and 3) \n"))
 
@@ -51,17 +50,18 @@ def user_answer():
                 print("Congratulations! You sunk the battleship, You Win!")
                 print_battleships_board(board)
                 turns = turns + 1
-                print(turns)
+                print("you are on guess" + str(turns) + " of 10")
                 break
             else:
                 board[guess_row][guess_col] = "O"
+                miss.append([guess_row, guess_col])
+                print(miss)
                 print("You Missed! Try Again")
                 print_battleships_board(board)
                 turns = turns + 1
-                print(turns)
-            
+                print("you are on guess " + str(turns) + " of 10")            
     except ValueError:
-        print("Invalid user input, try again")    
+        print("Invalid user input, try again")
 
 
 def main():
@@ -69,8 +69,6 @@ def main():
     print("Welcome to Battleships!\n")
     print("You get 10 bullets to play against the computer")
     print_battleships_board(board)
-    random_row(board)
-    random_col(board)
     user_answer()
 
 
